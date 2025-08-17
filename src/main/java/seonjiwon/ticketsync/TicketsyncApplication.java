e7c87b588d3aa8fe22d5132455dc49f2147cb7a3
seonjiwon <tommys915@gmail.com>
2025-08-17 21:22:39 +0900

    Initial commit

---
+++ src/main/java/seonjiwon/ticketsync/TicketsyncApplication.java
@@ -0,0 +1,13 @@
+package seonjiwon.ticketsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seonjiwon/ticketsync/TicketsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package seonjiwon.ticketsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
